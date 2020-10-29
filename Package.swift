9af534e2c57ca63b0166b7beb2c45bcd1e93744a
Spencer Whyte <spencerwhyte@gmail.com>
2020-10-28 21:42:22 -0400

    Added public headers to swift package

--- Package.swift
+++ Package.swift
@@ -17,6 +17,7 @@ let package = Package(
     targets: [
         .target(
             name: "SlackTextViewController",
-            path: "Source")
+            path: "Source",
+            publicHeadersPath: "Source")
     ]
 )
