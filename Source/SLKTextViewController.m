e92f7a4fb2087d56da67d18c0a476a42c3275b76
Spencer Whyte <spencer@probuild.app>
2026-02-25 17:06:40 -0500

    Revert keyboardLayoutGuide in favor of original VFL bottom constraint
    The keyboardLayoutGuide constraint made keyboardHC nil, breaking SLK's
    keyboard notification handler which relies on updating keyboardHC.constant
    to position the textInputbar above the keyboard. This caused the toolbar
    to be hidden behind the keyboard on first presentation.

--- Source/SLKTextViewController.m
+++ Source/SLKTextViewController.m
@@ -2258,9 +2258,7 @@ CGFloat const SLKAutoCompletionViewDefaultHeight = 140.0;
                             @"textInputbar": self.textInputbar
                             };
     
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[scrollView(0@750)][typingIndicatorView(0)]-0@999-[textInputbar(0)]" options:0 metrics:nil views:views]];
-
-    [self.textInputbar.bottomAnchor constraintEqualToAnchor: self.view.keyboardLayoutGuide.topAnchor].active = YES;
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[scrollView(0@750)][typingIndicatorView(0)]-0@999-[textInputbar(0)]|" options:0 metrics:nil views:views]];
 
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(>=0)-[autoCompletionView(0@750)][typingIndicatorView]" options:0 metrics:nil views:views]];
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[scrollView]|" options:0 metrics:nil views:views]];
